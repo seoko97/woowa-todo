8086a01ec3990eed0e54e05c9f822d4904e20500
sangrimlee <contact@sangrim.dev>
2022-07-13 16:04:30 +0900

    feat: LogItem 컴포넌트 구현
    - LogList에서 LogItem을 분리하여 구현
    - #9

---
+++ public/js/components/LogItem/index.js
@@ -0,0 +1,61 @@
+import "./style.css";
+import Component from "../component";
+
+export default class LogItem extends Component {
+  constructor($parent, $state) {
+    super($parent, "li", { class: "log-item" }, $state);
+
+    this.render();
+  }
+
+  getActionMessage(actionType) {
+    const actionMessage = {
+      ADD: "등록",
+      UPDATE: "변경",
+      DELETE: "삭제",
+      MOVE: "수정",
+    };
+
+    return actionMessage[actionType];
+  }
+
+  createLogContentTemplate(log) {
+    const { actionType, from, to, section, todoTitle } = log;
+
+    if (actionType === "MOVE") {
+      return `
+        <p class="log-item__content">
+          <strong>${todoTitle}</strong>을(를)
+          <strong>${from}</strong>에서
+          <strong>${to}</strong>로
+          <strong>${this.getActionMessage(actionType)}</strong>하였습니다.
+        </p>
+      `;
+    }
+
+    return `
+      <p class="log-item__content">
+        <strong>${section}</strong>에
+        <strong>${todoTitle}</strong>을(를)
+        <strong>${this.getActionMessage(actionType)}</strong>하였습니다.
+      </p>
+    `;
+  }
+
+  template() {
+    const { log } = this.$state;
+    return `
+      <span class="log-item__username">
+        @${log.username}
+      </span>
+      ${this.createLogContentTemplate(log)}
+      <span class="log-item__created-at">
+        ${log.createdAt}
+      </span>
+    `;
+  }
+
+  mount() {
+    this.$element.innerHTML = this.template();
+  }
+}
