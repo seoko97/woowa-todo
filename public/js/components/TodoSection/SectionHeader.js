31ec02808b470c618775ac0ccda8d8293267a0e6
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-13 13:08:34 +0900

    feat: TodoSection Component 생성
    - DB에서 받아온 데이터에 따라 Section todtjd
    - #11

---
+++ public/js/components/TodoSection/SectionHeader.js
@@ -0,0 +1,29 @@
+import Component from "../component";
+import PlusIcon from "../../../images/plus.svg";
+import CloseIcon from "../../../images/close.svg";
+
+export default class SectionHeader extends Component {
+  constructor($parent, $state, $props) {
+    super($parent, "div", { class: "todo-section-header" }, $state, $props);
+
+    this.render();
+  }
+
+  mount() {
+    const { title, todos } = this.$state;
+    this.$element.innerHTML = `
+          <div class="todo-section-header__right">
+            <h3>${title}</h3>
+            <span>${
+              todos.filter((todo) => todo.status !== "CREATE").length
+            }</span>
+          </div>
+          <span class="icon plus">
+            ${PlusIcon}
+          </span>
+          <span class="icon close">
+            ${CloseIcon}
+          </span>
+    `;
+  }
+}
