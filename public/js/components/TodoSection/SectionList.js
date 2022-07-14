fb743b260f3c21e28fc97aaa6df9517d5bc5b193
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-14 13:15:51 +0900

    refactor: 이벤트 위임 관리 위치 변경 - 기존 TodoSection에서 관리던 button 이벤트를 한단계 하위 컴포넌트인 TodoList에서 관리 - Status에 따라 Component 변경 및 이벤트 수정 - #11

--- public/js/components/TodoSection/SectionList.js
+++ public/js/components/TodoSection/SectionList.js
@@ -1,31 +1,103 @@
 import Component from "../component";
-import TodoItem from "../TodoItem";
+import BaseItem from "../TodoItem/BaseItem";
+import InputItem from "../TodoItem/InputItem";
 
 export default class SectionList extends Component {
+  $todos;
+
   constructor($parent, $state, $props) {
     super($parent, "ul", { class: "todo-section-ul" }, $state, $props);
 
     this.render();
   }
 
-  setEvent() {}
+  setEvent() {
+    this.$element.removeEventListener("click", this.onClickItem.bind(this));
+    this.addEvent("click", ".todo-item", this.onClickItem.bind(this));
+  }
+
+  onClickItem(e) {
+    const $item = e.target.closest(".todo-item");
+    const $button = e.target.closest(".button");
+
+    if (!$button || !$item) return;
+
+    const itemState = this.$state.todos.find(
+      (todo) => todo.id === $item.dataset.id
+    );
+
+    if (itemState?.status === "EDIT") this.toggleButtonByEdit(itemState.id, e);
+    else this.toggleButtonByCreate(e);
+  }
+
+  toggleButtonByCreate(e) {
+    if (e.target.classList.contains("cancle")) {
+      document.querySelector(".active").classList.remove("active");
+      this.mount();
+    } else if (e.target.classList.contains("submit")) {
+      // 상위 state 변경 로직 있어야함
+      console.log("@@@");
+    }
+  }
+
+  toggleButtonByEdit(id, e) {
+    const newState = { ...this.$state };
+
+    if (e.target.classList.contains("cancle")) {
+      const todoState = newState.todos.find((todo) => todo.id === id);
+      todoState.status = "DEFAULT";
+      this.setState(newState);
+    } else if (e.target.classList.contains("submit")) {
+      // 상위 state 변경 로직 있어야함
+      console.log("@@@");
+    }
+  }
 
   mount() {
+    this.$element.innerHTML = "";
     const { todos } = this.$state;
+    const $start = document.createElement("li");
+    $start.classList = "start";
 
-    new TodoItem(
+    new InputItem(
       this.$element,
-      { class: "todo-item create" },
+      {
+        class: "todo-item create focus",
+      },
       { status: "CREATE" },
-      { addTodoItem: this.$props.addTodoItem }
+      {
+        addTodoItem: this.$props.addTodoItem,
+        rerender: this.mount.bind(this),
+      }
     );
-    todos.forEach((todo) => {
-      new TodoItem(
-        this.$element,
-        { class: "todo-item" },
-        { todo, status: "DEFAULT" },
-        {}
-      );
+
+    this.$element.append($start);
+
+    this.$todos = todos.map((todo, i) => {
+      switch (todo.status) {
+        case "DEFAULT":
+          return new BaseItem(
+            this.$element,
+            {
+              class: "todo-item default",
+              "data-id": todo.id,
+            },
+            todo,
+            {}
+          );
+        case "EDIT":
+          return new InputItem(
+            this.$element,
+            {
+              class: "todo-item edit",
+              "data-id": todo.id,
+            },
+            todo,
+            {
+              rerender: this.mount.bind(this),
+            }
+          );
+      }
     });
   }
 }
