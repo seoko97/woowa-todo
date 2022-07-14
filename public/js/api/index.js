2c4ffb19e310616d839115dcf50ffa4310480987
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-14 21:35:20 +0900

    feat: api handler 생성 - url, method, body(data)를 입력받아 실행하는 함수 - 공통된 로직을 하나의 함수에서 관리 - #32

---
+++ public/js/api/index.js
@@ -0,0 +1,13 @@
+export const request = (url, method, body) => {
+  const config = { method };
+
+  if (body) config.body = JSON.stringify(body);
+
+  return fetch(`/api${url}`, config)
+    .then((res) => {
+      if (!res.ok) throw new Error("알 수 없는 에러가 발생하였습니다.");
+
+      return res.json();
+    })
+    .catch((e) => e);
+};
